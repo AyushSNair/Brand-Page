16c386901b4e98d7af348f42696e1e52921d5f8d
Ayush <nairayush45@gmail.com>
2025-01-11 12:37:38 +0530

    Initial Commit

---
+++ brand-page/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import MyComponent from './Components/Navbar';
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <MyComponent/>
+  )
+}
+
+export default App
---
+++ brand-page/src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import '../App.css'
+function MyComponent(){
+    return(
+        <div >
+            <nav >
+                <div>
+                    <img src='/images/brand_logo.png' />
+                </div>
+                <ul>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+                </ul>
+
+                <button>Login</button>
+            </nav>
+        </div>
+    )
+}
+
+export default MyComponent;
